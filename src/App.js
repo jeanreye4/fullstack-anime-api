cbf03d7d0e68a751bc1f3f45fa1a577c2b3c9fb7
Jean Reyes <jeanreyes@ntc-mba13-ca1475.myfiosgateway.com>
2022-10-31 19:16:32 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { animedata } from "./data.js"
+import buttons from "./buttons.js"
 
-function App() {
+function Anime() {
+  console.log(animedata)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <h1>Anime Quote API</h1>
+      <div class="all-quotes">
+      <div class="All-Might">
+        <button id="onebutton" onClick="background1()">All Might</button>
+      </div>
+      <div class="Eraserhead">
+        <button id="twobutton" onClick="background2()">Eraserhead</button>
+      </div>
+      <div class="Shigaraki-Tomura">
+        <button id="threebutton" onClick="background3()">Shigaraki Tomura</button>
+      </div>
+      <div class="Midoriya-Izuku">
+        <button id="fourbutton" onClick="background4()">Midoriya Izuku</button>
+      </div>
+      <div class="Bakugo-Katsuki">
+        <button id="button" onClick="background5()">Bakugo Katsuki</button>
+      </div>
+      <div class="Levi-Ackerman">
+        <button id="button" onClick="background6()">Levi Ackerman</button>
+      </div>
+      <div class="Escanor">
+        <button id="button" onClick="background7()">Escanor</button>
+      </div>
+      <div class="meliodas">
+        <button id="button" onClick="background8()">Meliodas</button>
+      </div>
+      <div class="Naurto-Uzumaki">
+          <button id="button" onClick="background9()">Naruto Uzumaki</button>
+      </div>
     </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default Anime;
